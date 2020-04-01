80cc2180f94893d97be3c529238f1dfbae2781fa
Prateek Sarangi <sarangiprateek80@gmail.com>
2020-04-01 13:28:07 +0530

    Object detection modified for generalized images

--- ObjectDetection.py
+++ ObjectDetection.py
@@ -2,17 +2,6 @@ import numpy as np
 import argparse
 import cv2
 
-ap = argparse.ArgumentParser()
-ap.add_argument("-i", "--image", required=True,
-	help="")
-ap.add_argument("-p", "--prototxt", required=True,
-	help="")
-ap.add_argument("-m", "--model", required=True,
-	help="")
-ap.add_argument("-c", "--confidence", type=float, default=0.2,
-	help="minimum probability to filter weak detections")
-args = vars(ap.parse_args())
-
 CLASSES = ["background", "aeroplane", "bicycle", "bird", "boat",
 	"bottle", "bus", "car", "cat", "chair", "cow", "diningtable",
 	"dog", "horse", "motorbike", "person", "pottedplant", "sheep",
@@ -20,11 +9,10 @@ CLASSES = ["background", "aeroplane", "bicycle", "bird", "boat",
 COLORS = np.random.uniform(0, 255, size=(len(CLASSES), 3))
 
 print("[INFO] loading model...")
-net = cv2.dnn.readNetFromCaffe(args["prototxt"], args["model"])
+net = cv2.dnn.readNetFromCaffe('MobileNetSSD_deploy.prototxt.txt', 'MobileNetSSD_deploy.caffemodel')
 
-image = cv2.imread(args["image"])
+image = cv2.imread('test.jpeg')
 (h, w, c) = image.shape
-print(args["image"])
 blob = cv2.dnn.blobFromImage(cv2.resize(image, (300, 300)), 0.007843, (300, 300), 127.5)
 
 print("[INFO] computing object detections...")
@@ -34,7 +22,7 @@ detections = net.forward()
 for i in np.arange(0, detections.shape[2]):
 	confidence = detections[0, 0, i, 2]
 
-	if confidence > args["confidence"]:
+	if confidence > 0.2:
 		idx = int(detections[0, 0, i, 1])
 		box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
 		(startX, startY, endX, endY) = box.astype("int")
